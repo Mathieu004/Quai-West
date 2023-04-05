5a1656735e63a87c5ff88b5e2fb0cce2690d4462
Mathieu Cazaux <matcaz33.mobile@gmail.com>
2023-04-05 19:02:36 +0200

    v2

--- docs/js/app.js
+++ docs/js/app.js
@@ -4,10 +4,49 @@ window.addEventListener('scroll', () => {
   img.style.transform = `translateY(-${window.scrollY * 1.5}px)`;
 });
 
+/*----------------------------------------------------------- MENU-NAVIGATION ----------------------------------------------------------------------------*/
 
-/*---------------------------------------------------------------- PLATS ---------------------------------------------------------------------------------*/
 
-fetch('docs/json/burger.json')
+const btnClassicBurger = document.querySelector('.btn-classic-burger');
+const ClassicBurger = document.querySelector('.option-classic-burger');
+
+const btnPremiumBurger = document.querySelector('.btn-premium-burger');
+const PremiumBurger = document.querySelector('.option-premium-burger');
+
+const btnAllStarBurger = document.querySelector('.btn-allstar-burger');
+const AllStarBurger = document.querySelector('.option-allstar-burger');
+
+btnClassicBurger.addEventListener('click', () => {
+  ClassicBurger.classList.remove('hidden');
+  PremiumBurger.classList.add('hidden');
+  AllStarBurger.classList.add('hidden');
+  btnClassicBurger.style.borderBottom = '2px solid #c89446';
+  btnPremiumBurger.style.borderBottom = 'none';
+  btnAllStarBurger.style.borderBottom = 'none';
+});
+
+btnPremiumBurger.addEventListener('click', () => {
+  PremiumBurger.classList.remove('hidden');
+  ClassicBurger.classList.add('hidden');
+  AllStarBurger.classList.add('hidden');
+  btnClassicBurger.style.borderBottom = 'none';
+  btnPremiumBurger.style.borderBottom = '2px solid #c89446';
+  btnAllStarBurger.style.borderBottom = 'none';
+});
+
+btnAllStarBurger.addEventListener('click', () => {
+  AllStarBurger.classList.remove('hidden');
+  ClassicBurger.classList.add('hidden');
+  PremiumBurger.classList.add('hidden');
+  btnClassicBurger.style.borderBottom = 'none';
+  btnPremiumBurger.style.borderBottom = 'none';
+  btnAllStarBurger.style.borderBottom = '2px solid #c89446';
+});
+
+
+/*---------------------------------------------------------------- BURGERS ---------------------------------------------------------------------------------*/
+
+fetch('docs/json/burgers.json')
   .then(response => response.json())
   .then(data => {
     data.forEach(element => {
@@ -35,8 +74,18 @@ fetch('docs/json/burger.json')
       description.textContent =  `${element.description == "" ? `Pas de description pour l'instant` : element.description == false ? `Pas de description pour l'instant` : element.description}`;
       div.appendChild(description);
 
-      document.querySelector('.menu').appendChild(fiche);
+      if(element.categorie === "Classic Burgers ✩") {
+        document.querySelector('.option-classic-burger').appendChild(fiche);
+      }
+
+      else if(element.categorie === "Premium Burgers ✩✩") {
+        document.querySelector('.option-premium-burger').appendChild(fiche);
+      }
+
+      else {
+        document.querySelector('.option-allstar-burger').appendChild(fiche);
+      }
+
     });
-  })
-  .catch(error => console.error(error));
+  }).catch(error => console.error(error));
 
